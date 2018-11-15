9d2bde6f95403761c701c24160e423e399ce817e
Martin Aumüller <aumuell@reserv.at>
2018-11-15 16:41:46 +0100

    pass DISPLAY

--- singularity.sh
+++ singularity.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
 # if mpirun does not launch all ranks via ssh, break out of container
-ssh localhost singularity "$@" || echo "Failed to launch singularity on localhost ($(hostname))"
+if [ -z "$DISPLAY" ]; then
+    ssh localhost singularity "$@" || echo "Failed to launch singularity on localhost ($(hostname))"
+else
+    ssh localhost "DISPLAY=$DISPLAY" singularity "$@" || echo "Failed to launch singularity on localhost ($(hostname))"
+fi
