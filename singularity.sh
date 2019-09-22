55e0f0f08502d7cac6be9820a540f74836820667
Martin Aumüller <aumuell@reserv.at>
2019-09-23 01:08:03 +0200

    no longer needed

--- singularity.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# if mpirun does not launch all ranks via ssh, break out of container
-if [ -z "$DISPLAY" ]; then
-    ssh localhost singularity "$@" || echo "Failed to launch singularity on localhost ($(hostname))"
-else
-    ssh localhost "DISPLAY=$DISPLAY" singularity "$@" || echo "Failed to launch singularity on localhost ($(hostname))"
-fi
